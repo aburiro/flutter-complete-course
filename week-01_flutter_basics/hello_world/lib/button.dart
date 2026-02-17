079cac06d95d71ae5ea82857623188425c377186
aburiro <96950982+aburiro@users.noreply.github.com>
2026-02-17 12:48:43 +0500

    button in flutter

---
+++ week-01_flutter_basics/hello_world/lib/button.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Mybutton extends StatelessWidget {
+  const Mybutton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(onPressed: () {}, child: Text("Click Me"));
+  }
+}
